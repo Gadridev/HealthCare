d9b44617c5a70983a5bc0cae90ebd45ba4759b27
Gadri Mohamed <gadrimohamed8@gmail.com>
2026-01-22 16:49:19 +0100

    feat:adding function showAllvalues

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
-    const nom = document.getElementById("nom");
-    const prenom = document.getElementById("prenom");
-    const email = document.getElementById("email");
-    const motif = document.getElementById("motif");
-    const date = document.getElementById("date");
-    const btn=document.getElementById('btn');
-    const error=document.getElementById('error-message');
-    console.log(nom,prenom)
-    function showAllvalues(nom, prenom, email, motif, date) {
-        if(!nom || !prenom || !email || !motif || !date){
-            setTimeout(() => {
-                showError("invalid input")
-            }, 1000);
-            error.classList.add('hidden')
-            return;
-        }
-        console.log(nom,prenom,email,motif,date)
-    }
-    function init() {
-    showAllvalues(nom.value, prenom.value,email.value, motif.value, date.value);
-    }
-    function showError(message) {
-    const errorDiv = document.getElementById("error-message");
-    errorDiv.textContent = message;
-    errorDiv.classList.remove("hidden");
-    }
-    btn.addEventListener("click", function(e){
-        e.preventDefault();
-        init();
+const nom = document.getElementById("nom");
+const prenom = document.getElementById("prenom");
+const email = document.getElementById("email");
+const motif = document.getElementById("motif");
+const date = document.getElementById("date");
+const btn = document.getElementById("btn");
+const error = document.getElementById("error-message");
 
-    });
+function showAllvalues(nom, prenom, email, motif, date) {
+  if (!nom || !prenom || !email || !motif || !date) {
+    showError("Tous les champs sont obligatoires");
+    return;
+  }
+
+  const storedData = JSON.parse(localStorage.getItem("data")) || [];
+  const exist = storedData.some(
+    (item) =>
+      item.email === email || item.nom === nom || item.prenom === prenom,
+  );
+  if (exist) {
+    showError(" email ou nom ou prenom existe déjà");
+    return;
+  }
+
+  const formData = { nom, prenom, email, motif, date };
+  storedData.push(formData);
+
+  localStorage.setItem("data", JSON.stringify(storedData));
+  window.location.href = "./table.html";
+}
+
+function showError(message) {
+  error.textContent = message;
+  error.classList.remove("hidden");
+
+  setTimeout(() => {
+    error.classList.add("hidden");
+  }, 1500);
+}
+
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  showAllvalues(
+    nom.value.trim(),
+    prenom.value.trim(),
+    email.value.trim(),
+    motif.value.trim(),
+    date.value.trim(),
+  );
+});
