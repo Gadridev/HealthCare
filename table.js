7b98a500d38c479bf6d32f34cfea20ec2d486453
Gadri Mohamed <gadrimohamed8@gmail.com>
2026-01-23 15:19:11 +0100

    adding html table and showing data in table

---
+++ table.js
@@ -0,0 +1,36 @@
+let getData = JSON.parse(localStorage.getItem("data") || "[]");
+let search=document.getElementById("search");
+let filterData = [...getData];
+function initTable() {
+  showData();
+}
+//first commit
+function showData() {
+  const container = document.querySelector("tbody");
+  container.innerHTML = "";
+
+  filterData.forEach((item, index) => {
+    const tr = document.createElement("tr");
+    tr.className = "bg-white border-b hover:bg-gray-50";
+
+    tr.innerHTML = `
+      <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+        ${item.nom}
+      </th>
+      <td class="px-6 py-4">${item.prenom}</td>
+      <td class="px-6 py-4">${item.email}</td>
+      <td class="px-6 py-4">${item.motif}</td>
+      <td class="px-6 py-4">${item.date}</td>
+      <td class="px-6 py-4 text-right">
+        <button
+          class="bg-blue-50 p-3 rounded-[15px] font-medium text-red-600 hover:underline"
+          onclick="deleteItem(${index})">
+          Delete
+        </button>
+      </td>
+    `;
+    container.appendChild(tr);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", initTable);
